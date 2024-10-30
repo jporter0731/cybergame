47e28bf48ab2ec70ef9aa431f1f131e3101f351d
James Porter <jporter@ecec.com>
2024-10-30 11:16:28 -0500

    Updated footer for spacing and to close out the database

--- private/footer.php
+++ private/footer.php
@@ -1,5 +1,10 @@
+<footer>
+
+</footer>
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
 <script src="js/scripts.js"></script>
 </html>
+
+<?php db_disconnect($db) ?>
