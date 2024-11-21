df47262652695b7cf3f5b2d4b1021acc7d4c023d
jporter0731 <79811536+jporter0731@users.noreply.github.com>
2024-11-21 14:34:48 -0600

    Update credentials.php
    changed the DB_SERVER value to be more reusable

--- private/credentials.php
+++ private/credentials.php
@@ -1,6 +1,8 @@
 <?php
-
-  define("DB_SERVER", "localhost");
+  // get db hostname from environment, otherwise set to localhost
+  $db_host = (getenv('MARIADB_HOST') !== false) ? getenv('MARIADB_HOST') : 'localhost';
+  
+  define("DB_SERVER", $db_host);
   define("DB_USER", "cybergame");
   define("DB_PASS", "zia8#My*uVw27Lg");
   define("DB_NAME", "cybergame");
